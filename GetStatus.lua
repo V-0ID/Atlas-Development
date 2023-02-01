3a439f58b1fd84e4cc17d941da507a8267ed43d7
Matteo <88525373+V-0ID@users.noreply.github.com>
2023-02-01 20:20:45 +1100

    Update GetStatus.lua

--- GetStatus.lua
+++ GetStatus.lua
@@ -1,2 +1,5 @@
-State = "W" -- W = Working | M = Minor Issues | N = Not Working
-Ver = "3.0.0"
+local _SCRIPTSTATE = {
+  State = "W"; -- W = Working | M = Minor Issues | N = Not Working
+  Ver = "3.0.0";
+}
+return _SCRIPTSTATE
