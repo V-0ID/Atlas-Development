d9fd0da49b33970277a15f4632c4816204e10ebe
Matteo <88525373+V-0ID@users.noreply.github.com>
2023-02-01 20:05:27 +1100

    Update GetStatus.lua

--- GetStatus.lua
+++ GetStatus.lua
@@ -1,2 +1,2 @@
-getgenv().State = "W"; -- W = Working | M = Minor Issues | N = Not Working
-getgenv().Ver = "3.0.0";
+State = "W"; -- W = Working | M = Minor Issues | N = Not Working
+Ver = "3.0.0";
